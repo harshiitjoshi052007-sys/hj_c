1296c533613384c70d9fd91d9a29b167a97bcf53
harshit joshi <harshiitjoshi052007@gmail.com>
2025-08-28 23:52:41 +0530

    Add files via upload

---
+++ 1-first.c
@@ -0,0 +1,8 @@
+#include<stdio.h> //pre processor directive //HEADER FILE
+int main() //entry point of the programme //MAIN
+{ //start of program
+    printf("Hello world"); //Prints everything written in the bracket
+    return 0; // rok do aur return karo 0 value
+}//end of the program
+//; to terminate the statement
+//stdio - standard input output library
