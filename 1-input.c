48ea603f925dd5eee8083d0274ac0a0a132d2828
harshit joshi <harshiitjoshi052007@gmail.com>
2025-08-28 23:56:17 +0530

    Add files via upload

---
+++ 1-input.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() //input lena hai to scanf use karo
+{
+    int a;
+    scanf("%d", &a); //&-ampercent address of opreator it means that jo value hune di hai vo value a ko supply hogi
+    printf("The value of a is %d",a);
+    return 0;
+}
---
+++ 2-escape_sequences.c
@@ -0,0 +1,17 @@
+/*
+1.\n - newline
+2.\"
+3.\\
+4.\?
+5.\a
+6.\'
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello world \n my name is harshit joshi\n");
+    printf("How are you\?\n");
+    printf("Hello my name is \"Harshit\"\n");
+    return 0;
+}
